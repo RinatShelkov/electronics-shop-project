5049fea0085fcfb687b5b5b156838d5800f67b50
rinat shelkov <rinarserov@icloud.com>
2023-12-12 22:55:21 +0500

    Homework_2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,9 @@
+import csv
+import re
+
+from data.config import ROOT_PATH
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,11 +19,39 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         Item.all.append(self)
+
+    @property
+    def name(self):
+        Item.all.append(self)
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        Item.all.append(self)
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            self.__name = name[0:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, file_csv):
+        Item.all.append(cls)
+        with open(ROOT_PATH.joinpath(file_csv), newline='') as csvfile:
+            results = csv.DictReader(csvfile)
+            for result in results:
+                print(result['name'], result['price'], result['quantity'])
+
+
+
+    @staticmethod
+    def string_to_number(number_string):
+        number = int(re.findall(r"\d+", number_string)[0])
+        return number
 
     def calculate_total_price(self) -> float:
         """
@@ -28,7 +62,7 @@ class Item:
 
         return self.price * self.quantity
 
-    def apply_discount(self) -> None:
+    def apply_discount(self) -> None | float:
         """
         Применяет установленную скидку для конкретного товара.
         """
--- tests/test_item.py
+++ tests/test_item.py
@@ -10,10 +10,36 @@ def item_1():
     return item1
 
 
-def test_Item_homework_1(item_1):
+def test_item_homework_1(item_1):
+    # Проверка calculate_total_price - получение общей стоимости конкретного товара в магазине
     assert item_1.calculate_total_price() == 200000
+
+    # Проверка изменения размера скидке в классе
     Item.pay_rate = 0.8
     assert Item.pay_rate == 0.8
 
+    # Проверка apply_discount() - применнение скидки для товара
     assert item_1.apply_discount() == 8000.0
+
+    # Проверка сколько экземпляров класса создано
     assert len(item_1.all) == 1
+
+    # Проверка изменения наименования с декоратором property и setr, длина слова до 10 символов
+    item_1.name = 'Смартфон'
+    assert item_1.name == 'Смартфон'
+
+    # Проверка изменения наименования с декоратором property и setr, длина слова больше 10 символов,
+    # обрезает  строку (оставляет первые 10 символов)
+    item_1.name = 'Смартфон123456'
+    assert item_1.name == 'Смартфон12'
+
+    # проверка string_to_number()` - статический метод, возвращающий число из числа-строки
+    assert Item.string_to_number('5') == 5
+
+    # Проверка работоспобоности @classmethod
+
+
+def test_from_csv(capsys):
+    Item.instantiate_from_csv('src/items.csv')
+    log_text = capsys.readouterr()
+    assert log_text.out.strip() == 'Смартфон 100 1\nНоутбук 1000 3\nКабель 10 5\nМышка 50 5\nКлавиатура 75 5'
